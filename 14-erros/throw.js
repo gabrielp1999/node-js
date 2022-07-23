f39debb01c44392f4af8bac6291908a0279a865e
Gabriel <gabriel.gsm2017@gmail.com>
2022-07-23 11:46:28 -0300

    try cath

---
+++ 14-erros/throw.js
@@ -0,0 +1,7 @@
+const x = 10;
+
+if (!Number.isInteger(x)) {
+  throw new Error("O valor de x não é um numero inteiro");
+}
+
+console.log("continuando o codigo...");
---
+++ 14-erros/try-catch.js
@@ -0,0 +1,6 @@
+const x = 5;
+try {
+  x = 2;
+} catch (err) {
+  console.log(`erro ${err}`);
+}
