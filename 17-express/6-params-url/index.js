33f23660d48846e9a39aeefb6e747a98dcfea51b
Gabriel <gabriel.gsm2017@gmail.com>
2022-07-30 21:52:01 -0300

    params-url

---
+++ 17-express/6-params-url/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+
+const path = require("path");
+
+const basePath = path.join(__dirname, "templates");
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+
+  console.log(`Estamos buscando pelo usuário: ${id}`);
+
+  res.sendFile(`${basePath}/users.html`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(`${basePath}/index.html`);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
