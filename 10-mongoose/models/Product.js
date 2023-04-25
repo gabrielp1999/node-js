e42812e3e84431453db94262a8f5c5238df9c912
Gabriel <[200~gabriel.souza@interativachat.gabriel.souza@interativachat.com.br>
2023-04-25 17:11:22 -0300

    feat: criando schema

--- 10-mongoose/models/Product.js
+++ 10-mongoose/models/Product.js
@@ -1,57 +1,14 @@
-const conn = require("../db/conn");
-const { ObjectId } = require("mongodb");
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-
-class Product {
-    constructor(name, description, price, image){
-        this.name = name
-        this.description = description
-        this.price = price
-        this.image = image
-    }
-
-    save(){
-        const newProduct = {
-            name: this.name,
-            description: this.description,
-            price: this.price,
-            image: this.image
-        };
-        const product = conn.db().collection("products").insertOne(newProduct)
-
-        return product;
-    }
-
-    static getAll(){
-        const products = conn.db().collection("products").find().toArray();
-        return products;
-    }
-
-    static async getProductById(id){
-
-        const product = await conn
-            .db()
-            .collection("products")
-            .findOne({_id: new ObjectId(id)});
-
-        return product;
-    }
-
-    static async deleteProductById(id){
-
-        const product = await conn
-        .db()
-        .collection("products")
-        .deleteOne({_id: new ObjectId(id)});
-
-        return product;
-    }
-
-    updateProduct(id){
-        const query = {$set: this};
-        conn.db().collection("products").updateOne({_id: new ObjectId(id)}, query)
-    }
-
-}
+const Product = mongoose.model(
+    'Product',
+    new Schema({
+        name: { type: String, required: true},
+        price: { type: Number, required: true },
+        description: { type: String, required: true },
+        image: { type: String, required: true }
+    })
+)
 
 module.exports = Product;
